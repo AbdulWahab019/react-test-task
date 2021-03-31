8e5e49be4496fcad5b40bbade8d1b98e606fc8ad
Muhammad Uzair <abdul.wahab@tkxel.com>
2021-03-31 13:38:29 +0500

    - Updated File

--- app/controllers/calculatorController.ts
+++ app/controllers/calculatorController.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express = require("express");
 const router = express.Router();
 import calculatorService from "../services/calculatorService";
 
-router.post("/sum", (req, res) => {
+router.post("/sum", (req: express.Request, res: express.Response) => {
   const { number1, number2 } = req.body;
   const resp = calculatorService.getSum(number1, number2);
 
